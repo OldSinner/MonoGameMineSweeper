6c5b0101b73c64cc3938ba607319bf28b17df474
OldSinner <antylifepatia@gmail.com>
2021-04-14 12:55:22 +0200

    Generate Number for titles

---
+++ src/MineSweeper/Engine/Utility.cs
@@ -0,0 +1,50 @@
+using System;
+
+namespace MineSweeper.Engine
+{
+    public static class Utility
+    {
+        public static int[,] GenerateMines(int[,] mines, int numberofmines, int bombHeight, int bombWidth)
+        {
+            var rand = new Random();
+            while (numberofmines != 0)
+            {
+                var i = rand.Next(bombHeight);
+                var j = rand.Next(bombWidth);
+                if (mines[i, j] != 10)
+                {
+                    mines[i, j] = 10;
+                    numberofmines--;
+                }
+            }
+            return mines;
+        }
+        public static int CheckType(int[,] minefield, int column, int row)
+        {
+            int type = 0;
+            if (CheckIfMines(minefield, column + 1, row)) type++;
+            if (CheckIfMines(minefield, column - 1, row)) type++;
+            if (CheckIfMines(minefield, column, row + 1)) type++;
+            if (CheckIfMines(minefield, column, row - 1)) type++;
+            if (CheckIfMines(minefield, column + 1, row - 1)) type++;
+            if (CheckIfMines(minefield, column + 1, row + 1)) type++;
+            if (CheckIfMines(minefield, column - 1, row + 1)) type++;
+            if (CheckIfMines(minefield, column - 1, row - 1)) type++;
+            return type;
+        }
+        public static bool CheckIfMines(int[,] minefiled, int column, int row)
+        {
+            try
+            {
+                if (minefiled[column, row] == 10) return true;
+                else return false;
+            }
+            catch(Exception x)
+            {
+                return false;
+            }
+
+
+        }
+    }
+}
--- src/MineSweeper/Game1.cs
+++ src/MineSweeper/Game1.cs
@@ -11,8 +11,8 @@ namespace MineSweeper
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-        static int bombHeight = 15;
-        static int bombWidth = 10;
+        static int bombHeight = 20;
+        static int bombWidth = 20;
         List<GameObject> GameObjTitles = new List<GameObject>();
 
         Vector2[,] titles = new Vector2[bombHeight,bombWidth];
@@ -41,21 +41,57 @@ namespace MineSweeper
 
         protected override void LoadContent()
         {
-            minefield = Generator.GenerateMines(minefield,56,bombHeight,bombWidth);
+            minefield = Utility.GenerateMines(minefield,100,bombHeight,bombWidth);
+             for(int i = 0;i<bombHeight;i++)
+            {
+                for(int j=0;j<bombWidth;j++)
+                {
+                    if(minefield[i,j]!=10)
+                    {
+                        minefield[i,j] = Utility.CheckType(minefield,i,j);
+                    }
+                   
+                }
+            }
+
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             for(int i = 0;i<bombHeight;i++)
             {
                 for(int j=0;j<bombWidth;j++)
                 {
-                    if(minefield[i,j]==0)
+                    switch(minefield[i,j])
                     {
+                        case 0:
                         GameObjTitles.Add(new GameObject(Content.Load<Texture2D>("blockzero"),new Vector2(j*minesSize,i*minesSize),0,0));
-                    }
-                    else
-                    {
+                        break;
+                        case 1:
+                        GameObjTitles.Add(new GameObject(Content.Load<Texture2D>("blockone"),new Vector2(j*minesSize,i*minesSize),0,0));
+                        break;
+                        case 2:
+                        GameObjTitles.Add(new GameObject(Content.Load<Texture2D>("blocktwo"),new Vector2(j*minesSize,i*minesSize),0,0));
+                        break;
+                        case 3:
+                        GameObjTitles.Add(new GameObject(Content.Load<Texture2D>("blockthree"),new Vector2(j*minesSize,i*minesSize),0,0));
+                        break;
+                        case 4:
+                        GameObjTitles.Add(new GameObject(Content.Load<Texture2D>("blockfour"),new Vector2(j*minesSize,i*minesSize),0,0));
+                        break;
+                        case 5:
+                        GameObjTitles.Add(new GameObject(Content.Load<Texture2D>("blockfive"),new Vector2(j*minesSize,i*minesSize),0,0));
+                        break;
+                        case 6:
+                        GameObjTitles.Add(new GameObject(Content.Load<Texture2D>("blocksix"),new Vector2(j*minesSize,i*minesSize),0,0));
+                        break;
+                        case 7:
+                        GameObjTitles.Add(new GameObject(Content.Load<Texture2D>("blockseven"),new Vector2(j*minesSize,i*minesSize),0,0));
+                        break;
+                        case 8:
+                        GameObjTitles.Add(new GameObject(Content.Load<Texture2D>("blockeight"),new Vector2(j*minesSize,i*minesSize),0,0));
+                        break;
+                        case 10:
                         GameObjTitles.Add(new GameObject(Content.Load<Texture2D>("blockbomb"),new Vector2(j*minesSize,i*minesSize),0,0));
+                        break;
                     }
-                    
                     titles[i,j] = new Vector2(j*minesSize,i*minesSize);
                 }
             }
